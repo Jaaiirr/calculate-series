3cb675a9f2daa726d7e4d09119b666d14606494b
Jair Eduardo <jair@facturatech.co>
2023-09-26 08:14:44 -0600

    SNG-009/Add test to calculate if a number provider is prime, create a method into utils folder

--- src/test/utils/expectedValues.js
+++ src/test/utils/expectedValues.js
@@ -9,4 +9,18 @@ const expectValueFibonacci = (n) => {
   return initialFibonacci.join(',');
 };
 
-export default expectValueFibonacci;
+const expectedValueIsPrime = (n) => {
+  if (n <= 1) return false;
+  if (n === 2) return true;
+
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) return false;
+  }
+
+  return true;
+};
+
+module.exports = {
+  expectValueFibonacci,
+  expectedValueIsPrime,
+};
